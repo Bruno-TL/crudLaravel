dcc8e7b894c9f5fe8304d34c2edae97c821e2b43
Thiago <tbgt35@hotmail.com>
2023-01-31 17:06:09 -0300

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::all();
+        return view('admin/posts/index', compact('posts'));
+    }
+}
+ 
---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/posts', [PostController::class, 'index']);
+
+Route::get('/', function () {
+    return view('welcome');
+});
