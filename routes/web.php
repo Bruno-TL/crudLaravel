59ee45930cba528a70511826a09fa8e80ee30467
Thiago <tbgt35@hotmail.com>
2023-02-06 10:24:47 -0300

    Auth2

--- routes/web.php
+++ routes/web.php
@@ -12,15 +12,17 @@ use App\Http\Controllers\PostController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::middleware(['auth'])->group(function (){
+    Route::any('/posts/search', [PostController::class, 'search'])->name('posts.search');
+    Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
+    Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
+    Route::delete('/posts/{id}',[PostController::class, 'destroy'])->name('posts.destroy');
+    Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/posts/created/create', [PostController::class, 'created'])->name('posts.created.create');
+    Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+});
 
-Route::any('/posts/search', [PostController::class, 'search'])->name('posts.search');
-Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
-Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
-Route::delete('/posts/{id}',[PostController::class, 'destroy'])->name('posts.destroy');
-Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-Route::get('/posts/created/create', [PostController::class, 'created'])->name('posts.created.create');
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 
 
 
